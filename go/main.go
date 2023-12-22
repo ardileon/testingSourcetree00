21e9e75ee871cbf294f3340daf8a6ec6022a8397
~~Leo <ardileon444@gmail.com>
2023-12-22 20:50:55 +0700

    add go

---
+++ go/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+)
+
+func main() {
+	var no int = 100
+	fmt.Println(reflect.TypeOf(no))
+
+	var intStr string = "100"
+	fourBaseEightBitInt, _ := strconv.ParseInt(intStr, 4, 8) // becomes no 16 and in\
+
+	tenBaseSixteenBitInt, _ := strconv.ParseInt(intStr, 10, 16) // no 100, and int64
+	fmt.Println(reflect.TypeOf(fourBaseEightBitInt))
+	fmt.Println(reflect.TypeOf(tenBaseSixteenBitInt))
+}
